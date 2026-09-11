1ca7004d98f6b8f4e50c0f4e7c4172f8334e0f12
yaushevzinur-cloud <yaushevzinur@gmail.com>
2026-09-11 13:25:12 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='myos-v01';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['/','/style.css','/app.js']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
